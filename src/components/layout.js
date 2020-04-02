d5bc5983e15cf7edc7d2f0deb8989f9b82783c76
pjsier <pjsier@gmail.com>
2020-04-02 11:32:12 -0500

    fix: fontawesome icon flashing

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import { library } from "@fortawesome/fontawesome-svg-core"
+import { config, library } from "@fortawesome/fontawesome-svg-core"
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons/faChevronUp"
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons/faExternalLinkAlt"
 import { faPhone } from "@fortawesome/free-solid-svg-icons/faPhone"
@@ -14,7 +14,12 @@ import { faLanguage } from "@fortawesome/free-solid-svg-icons/faLanguage"
 import Header from "./header"
 import Footer from "./footer"
 
+import "@fortawesome/fontawesome-svg-core/styles.css"
 import "../styles/style.scss"
+
+// Fix FontAwesome icon flashing
+// https://medium.com/@fabianterh/fixing-flashing-huge-font-awesome-icons-on-a-gatsby-static-site-787e1cfb3a18
+config.autoAddCss = false
 
 library.add(
   faChevronUp,
