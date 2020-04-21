6004f268f6fa4257e7a89f0ef0359f1d45ad2710
pjsier <pjsier@gmail.com>
2020-04-21 18:06:35 -0500

    feat: add updated tagalog translation

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require(`dotenv`).config()
 
 const siteUrl = process.env.SITE_URL || `https://covid.citybureau.org`
 
-const languages = [`en`, `es`, `pl`, `vi`]
+const languages = [`en`, `es`, `pl`, `tl`, `vi`]
 
 module.exports = {
   siteMetadata: {
